44a9f22116fd0d30a653e3bed1d5ea0c93f8dc96
HP <HP@Akshith>
2022-09-27 13:08:03 +0530

    first commit

---
+++ Springioc/src/main/java/com/tns/springioc/Airtel.java
@@ -0,0 +1,16 @@
+package com.tns.springioc;
+
+public class Airtel implements Sim {
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("calling using Airtel sim");
+	}
+
+	public void data() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Data Browsing using Airtel sim.");
+	}
+
+}
