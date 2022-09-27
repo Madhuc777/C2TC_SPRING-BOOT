eaed5d804528f1289cdd6adbdddea77ae1357fa1
HP <HP@Akshith>
2022-09-27 13:16:18 +0530

    first commt

---
+++ SpringDependencyInjection/src/main/java/com/tns/di1/Student.java
@@ -0,0 +1,13 @@
+package com.tns.di1;
+
+public class Student {
+
+	MathCheat chit;
+	
+	public void setChit(MathCheat chit) {
+		this.chit = chit;
+	}
+	public void cheating() {
+		chit.mathCheat();
+	}
+}
